eb60383c6317cb8579d67fcc2bfdb6d5336445ef
Will Holland <snowy.the.crow@gmail.com>
2020-03-13 20:34:40 +0700

    keybindings to change the pen
    ctrl+ to increase pen size
    ctrl- to decrase
    ctrl and a number to change colour

--- drawpad.rkt
+++ drawpad.rkt
@@ -1,9 +1,20 @@
 #lang racket/gui
 
+(define pink (make-color #xff #x69 #xb4))
+(define cyan (make-color #x00 #xff #xff))
+(define yellow (make-color #xff #xff #x00))
+(define red (make-color #xff #x45 #x00))
+(define green (make-color #x7f #xff #x00))
+(define purple (make-color #x94 #x00 #xd3))
+(define white (make-color #xff #xff #xff))
+(define grey (make-color #x82 #x82 #x82))
+
 (struct coord (x y))
 
 (define (event->coord event)
    (coord (send event get-x) (send event get-y)))
+
+(define pen (new pen% [color "BLACK"] [width 1]))
 
 (define frame (new frame% [label "Drawing"]
                    [min-width 300]
@@ -37,12 +48,69 @@
 
     ; keyboard events
     (define/override (on-char event)
-      (send frame set-status-text "Canvas keyboard."))
+      (when (send event get-control-down)
+        (cond ((equal? (send event get-key-code) #\=)
+               (set! pen (new pen%
+                              [color (send pen get-color)]
+                              [width (+ (send pen get-width) 1)]))
+               (send frame set-status-text "Pen size +"))
+              ((equal? (send event get-key-code) #\-)
+               (set! pen (new pen%
+                              [color (send pen get-color)]
+                              [width (max 0 (- (send pen get-width) 1))]))
+               (send frame set-status-text "Pen size -"))
+              ((equal? (send event get-key-code) #\1)
+               (set! pen (new pen%
+                              [color pink]
+                              [width (send pen get-width)]))
+               (send frame set-status-text "Pen color pink"))
+              ((equal? (send event get-key-code) #\2)
+               (set! pen (new pen%
+                              [color cyan]
+                              [width (send pen get-width)]))
+               (send frame set-status-text "Pen color cyan"))
+              ((equal? (send event get-key-code) #\3)
+               (set! pen (new pen%
+                              [color yellow]
+                              [width (send pen get-width)]))
+               (send frame set-status-text "Pen color yellow"))
+              ((equal? (send event get-key-code) #\4)
+               (set! pen (new pen%
+                              [color red]
+                              [width (send pen get-width)]))
+               (send frame set-status-text "Pen color red"))
+              ((equal? (send event get-key-code) #\5)
+               (set! pen (new pen%
+                              [color green]
+                              [width (send pen get-width)]))
+               (send frame set-status-text "Pen color green"))
+              ((equal? (send event get-key-code) #\6)
+               (set! pen (new pen%
+                              [color purple]
+                              [width (send pen get-width)]))
+               (send frame set-status-text "Pen color purple"))
+              ((equal? (send event get-key-code) #\7)
+               (set! pen (new pen%
+                              [color white]
+                              [width (send pen get-width)]))
+               (send frame set-status-text "Pen color white"))
+              ((equal? (send event get-key-code) #\8)
+               (set! pen (new pen%
+                              [color grey]
+                              [width (send pen get-width)]))
+               (send frame set-status-text "Pen color grey"))
+              ((equal? (send event get-key-code) #\9)
+               (set! pen (new pen%
+                              [color "BLACK"]
+                              [width (send pen get-width)]))
+               (send frame set-status-text "Pen color black")))
+        (send (send canvas get-dc) set-pen pen)))
     
     (super-new)))
 
 ; Make the drawing area
 (define canvas (new my-canvas% [parent frame]))
+(send (send canvas get-dc) set-pen pen) 
 
 (define menu-bar (new menu-bar% [parent frame]))
 
